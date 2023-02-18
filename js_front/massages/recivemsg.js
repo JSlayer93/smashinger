b6c24d436b14f99b851e9040bbe8396ebe9f8246
JSlayer93 <100201154+JSlayer93@users.noreply.github.com>
2023-02-18 19:42:26 +0400

    Update recivemsg.js

--- js_front/massages/recivemsg.js
+++ js_front/massages/recivemsg.js
@@ -4,21 +4,14 @@ import { createOtherMsg } from "./openMsg.js"
 
 
 export const reciveMsg = () => {
-<<<<<<< HEAD
     socket.on("reciveMsg", (msg, id, hisid) => {
         sendNotif(hisid)
         if(localStorage.getItem("id") == id && localStorage.getItem("Msgid") == hisid && localStorage.getItem("Msgid")){
-=======
-    socket.on("reciveMsg", (msg, name, hisname) => {
-        sendNotif(hisname)
-        if(localStorage.getItem("name") == name && localStorage.getItem("Msgname") == hisname && localStorage.getItem("Msgname")){
->>>>>>> origin/main
             createOtherMsg(msg)
         }
     })
 }
 
-<<<<<<< HEAD
 export const sendNotif = (id) => {
     Notification.requestPermission().then(perm => {
         if(perm === "granted" && document.visibilityState == "hidden"){
@@ -28,14 +21,3 @@ export const sendNotif = (id) => {
         }
     })
 }
-=======
-export const sendNotif = (name) => {
-    Notification.requestPermission().then(perm => {
-        if(perm === "granted" && document.visibilityState == "hidden"){
-            new Notification("MSGrecived", {
-                body: `you recived massage from ${name}`
-            })
-        }
-    })
-}
->>>>>>> origin/main
